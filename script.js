47a1beee551e496b30f616f3ccb652fe114adfcf
hypham97 <43606915+hypham97@users.noreply.github.com>
2018-10-08 18:11:41 +0300

    challenges-week-4-hypham97-master

---
+++ script.js
@@ -0,0 +1,76 @@
+/* #6 start the #external #action and say hello */
+console.log("App is alive");
+
+var currentLocation = 
+{
+     longitude: "E013° 22.166",
+     latitude: "N52° 31.212",
+     what3words:"dignity.rock.cakes"
+};
+/**
+ * #6 #Switcher function for the #channels name in the right app bar
+ * @param channel Channel Object
+ */
+function switchChannel(channel) {
+    //Log the channel switch
+    console.log("Tuning in to channel", channel);
+
+    currentChannel = channel;
+
+    //Write the new channel to the right app bar
+    document.getElementById('channel-name').innerHTML = channel.name;
+
+    //#6 change the #channel #location
+    var location ="by <a href=\"http://w3w.co/" + channel.createdBy +"\" target=\"_blank\"><strong>" + channel.createdBy + "</strong></a>";
+
+    document.getElementById('channel-location').innerHTML = location;
+    /* #6 #liking channels on #click */
+    console.log (document.getElementById('channel-star').outerHTML);
+
+    $('#channel-star').removeClass('fa-star far');
+    $('#channel-star').removeClass('fa-star fas');
+
+
+    channel.starred ? 
+    $('#channel-star').addClass('fas')
+    :
+    $('#channel-star').addClass('far');
+
+    $('#channel-star').addClass('fa-star');
+
+    /* #6 #highlight the selected #channel.
+       This is inefficient (jQuery has to search all channel list items), but we'll change it later on */
+    $('#channels li').removeClass('selected');
+    $('#channels li:contains(' + channel.name + ')').addClass('selected');
+}
+
+/* #6 #liking a channel on #click */
+function star() {
+    $('#channel-star').toggleClass("fas");
+    $('#channel-star').toggleClass("far");
+
+    currentChannel.starred = ! currentChannel.starred;
+}
+
+/**
+ * #6 #taptab selects the given tab
+ * @param tabId #id of the tab
+ */
+function selectTab(tabId) {
+    // #6 #taptab #remove selection from all buttons...
+    $('#tab-bar button').removeClass('selected');
+
+    //...#6 #taptab #log the new tab on change...
+    console.log('Changing to tab', tabId);
+
+    //...#6 #taptab #add selection to the given tab button, its id is passed via the #argument tabId
+    $(tabId).addClass('selected');
+}
+
+/**
+ * #6 #toggle (show/hide) the emojis menu #smile
+ */
+function toggleEmojis() {
+    /* $('#emojis').show(); // #show */
+    $('#emojis').toggle(); // #toggle
+}
